178405198706642fb385a18b71ac59f10e9714c9
fsarachu <fsarachu@gmail.com>
2017-03-22 22:23:27 -0300

    Write CategoriesTableSeeder

--- database/seeds/CategoriesTableSeeder.php
+++ database/seeds/CategoriesTableSeeder.php
@@ -11,6 +11,30 @@ class CategoriesTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+
+        DB::table('categories')->insert([
+            'name' => 'Transporte'
+        ]);
+
+        DB::table('categories')->insert([
+            'name' => 'Alimentos'
+        ]);
+
+        DB::table('categories')->insert([
+            'name' => 'Alojamiento'
+        ]);
+
+        DB::table('categories')->insert([
+            'name' => 'Servicios'
+        ]);
+
+        DB::table('categories')->insert([
+            'name' => 'Promoción'
+        ]);
+
+        DB::table('categories')->insert([
+            'name' => 'Otros'
+        ]);
+
     }
 }
