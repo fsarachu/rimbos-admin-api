d6e136a5868bf5f41f69bd58437574a24914d5e2
fsarachu <fsarachu@gmail.com>
2017-03-22 23:24:34 -0300

    Add index view

---
+++ resources/views/index.blade.php
@@ -0,0 +1,21 @@
+@extends('layout')
+
+@section('title', 'Inicio')
+
+@section('content')
+    <div class="ui grid text container">
+        <div class="row">
+            <div class="column">
+                <h2 class="ui horizontal divider header">Comprobantes</h2>
+            </div>
+        </div>
+        <div class="two column row">
+            <div class="column">
+                <a href="/invoices/new" class="ui primary fluid button">Cargar</a>
+            </div>
+            <div class="column">
+                <a href="/invoices" class="ui secondary fluid button">Listar</a>
+            </div>
+        </div>
+    </div>
+@endsection
