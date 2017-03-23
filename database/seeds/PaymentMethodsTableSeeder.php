ad2903212a164fa58939cc54e4441b045872043f
fsarachu <fsarachu@gmail.com>
2017-03-22 22:28:47 -0300

    Write PaymentMethodsTableSeeder

--- database/seeds/PaymentMethodsTableSeeder.php
+++ database/seeds/PaymentMethodsTableSeeder.php
@@ -11,6 +11,42 @@ class PaymentMethodsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+
+        DB::table('payment_methods')->insert([
+            'name' => 'Efectivo (Moneda Local)'
+        ]);
+
+        DB::table('payment_methods')->insert([
+            'name' => 'Efectivo (U$S)'
+        ]);
+
+        DB::table('payment_methods')->insert([
+            'name' => 'Tarjeta de crédito MD'
+        ]);
+
+        DB::table('payment_methods')->insert([
+            'name' => 'Tarjeta de crédito MD LUPS'
+        ]);
+
+        DB::table('payment_methods')->insert([
+            'name' => 'Tarjeta de débito ITAU'
+        ]);
+
+        DB::table('payment_methods')->insert([
+            'name' => 'Banco BROU'
+        ]);
+
+        DB::table('payment_methods')->insert([
+            'name' => 'Banco ITAU'
+        ]);
+
+        DB::table('payment_methods')->insert([
+            'name' => 'Paypal'
+        ]);
+
+        DB::table('payment_methods')->insert([
+            'name' => 'Otro'
+        ]);
+
     }
 }
