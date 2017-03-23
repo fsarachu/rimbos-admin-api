2be57f26c01a376bc9131892f8ba091547c876b1
fsarachu <fsarachu@gmail.com>
2017-03-23 02:02:58 -0300

    Use route names for  links href

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,10 +11,10 @@
         </div>
         <div class="two column row">
             <div class="column">
-                <a href="/invoices/new" class="ui primary fluid button">Cargar</a>
+                <a href="{{ route('invoices.create') }}" class="ui primary fluid button">Cargar</a>
             </div>
             <div class="column">
-                <a href="/invoices" class="ui secondary fluid button">Listar</a>
+                <a href="{{ route('invoices.index') }}" class="ui secondary fluid button">Listar</a>
             </div>
         </div>
     </div>
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -12,8 +12,8 @@
 
 <div id="sidebar-menu" class="ui left borderless vertical menu sidebar">
     <div class="header item">Comprobantes</div>
-    <a class="item" href="/invoices/new">Cargar</a>
-    <a class="item" href="/invoices">Listar</a>
+    <a class="item" href="{{ route('invoices.create')  }}">Cargar</a>
+    <a class="item" href="{{ route('invoices.index') }}">Listar</a>
 </div>
 
 <div class="ui top fixed red borderless inverted massive main menu">
