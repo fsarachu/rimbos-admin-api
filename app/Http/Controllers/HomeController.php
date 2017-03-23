f8e5aace7af3917f5f69e838238c97a34a45c0e7
fsarachu <fsarachu@gmail.com>
2017-03-23 01:38:53 -0300

    Add HomeController

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Display homepage.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke()
+    {
+
+        return view('index');
+    }
+}
