736dbd3f57149ac68e75cd969a6b1f5482456f62
fsarachu <fsarachu@gmail.com>
2017-03-22 22:04:31 -0300

    Write create_invoices_table migration

--- database/migrations/2017_03_23_005346_create_invoices_table.php
+++ database/migrations/2017_03_23_005346_create_invoices_table.php
@@ -15,6 +15,23 @@ class CreateInvoicesTable extends Migration
     {
         Schema::create('invoices', function (Blueprint $table) {
             $table->increments('id');
+            $table->date('date');
+            $table->string('trip');
+            $table->integer('country_id');
+            $table->text('description');
+            $table->string('business_name');
+            $table->string('invoice_number');
+            $table->integer('category_id');
+            $table->integer('payment_method_id');
+            $table->integer('currency_id');
+            $table->decimal('amount_in_original_currency');
+            $table->decimal('one_dollar_rate');
+            $table->decimal('amount_in_dollars');
+            $table->decimal('actual_paid_amount');
+            $table->string('image_url');
+            $table->boolean('include_rut')->default(false);
+            $table->boolean('assign_anii')->default(false);
+            $table->boolean('personal_spending')->default(false);
             $table->timestamps();
         });
     }
