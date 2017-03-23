ea3b9fa93e70d88ab84f9905f73080c51d8141cd
fsarachu <fsarachu@gmail.com>
2017-03-23 11:01:44 -0300

    Restructure view files

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,22 @@
+<html lang="es">
+<head>
+
+    @include('layouts.meta')
+    <link rel="apple-touch-icon" href="apple-touch-icon.png">
+    <title>Rimbos Admin - @yield('title')</title>
+    <link rel="stylesheet" href="/css/app.css">
+
+</head>
+<body>
+
+@include('layouts.nav')
+
+<div class="pusher">
+    @yield('content')
+</div>
+
+<script async src="/js/app.js"></script>
+@yield('scripts')
+
+</body>
+</html>
---
+++ resources/views/layouts/nav.blade.php
@@ -0,0 +1,12 @@
+<div id="sidebar-menu" class="ui left borderless vertical menu sidebar">
+    <div class="header item">Comprobantes</div>
+    <a class="item" href="{{ route('invoices.create')  }}">Cargar</a>
+    <a class="item" href="{{ route('invoices.index') }}">Listar</a>
+</div>
+
+<div class="ui top fixed red borderless inverted massive main menu">
+    <div id="sidebar-toggle" class="item">
+        <i class="sidebar icon"></i>
+    </div>
+    <div class="brand item"><a href="/">· RimbosAdmin ·</a></div>
+</div>
