4e7950b0dd21202919ec0f2645feb53003e9a363
fsarachu <fsarachu@gmail.com>
2017-03-23 21:19:40 -0300

    Add 404 error view

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.main')
+
+@section('title', '404')
+
+@section('content')
+    <div class="ui center aligned text container">
+        <h1 class="ui horizontal header divider">404</h1>
+    </div>
+@endsection
