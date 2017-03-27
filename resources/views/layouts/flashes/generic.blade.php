ac71d038fbdc9945cfb6e66940af572f580823a7
fsarachu <fsarachu@gmail.com>
2017-03-26 21:10:07 -0300

    Split flash messages into two partial views, one for form errors and other for generic messages

--- resources/views/layouts/flashes/generic.blade.php
+++ resources/views/layouts/flashes/generic.blade.php
@@ -15,17 +15,3 @@
         </div>
     </div>
 @endif
-
-@if (count($errors) > 0)
-    <div class="ui text container">
-        <div class="ui negative message">
-            <i class="close icon"></i>
-            <div class="header">Datos inválidos!</div>
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    </div>
-@endif
