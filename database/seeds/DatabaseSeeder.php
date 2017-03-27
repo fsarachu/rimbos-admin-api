83120a66f651efe629f250896c2632554e20cced
fsarachu <fsarachu@gmail.com>
2017-03-27 14:29:48 -0300

    Add UsersTableSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,5 +15,6 @@ class DatabaseSeeder extends Seeder
         $this->call(CountriesTableSeeder::class);
         $this->call(CurrenciesTableSeeder::class);
         $this->call(PaymentMethodsTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
     }
 }
