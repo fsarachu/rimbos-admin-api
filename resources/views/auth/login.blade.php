b5f964e2fd58d866b1267cf65ea6e8358d78df0d
fsarachu <fsarachu@gmail.com>
2017-03-26 21:12:55 -0300

    Improve login form layout

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,101 +1,35 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="ui middle aligned center aligned grid text  container">
+    <div class="ui center aligned grid small container">
         <div class="column">
-            <h2 class="ui header">Login</h2>
+            <div class="ui segment">
+                <form class="ui login form" method="POST" action="{{ route('login') }}">
 
-            <form class="ui large form" method="POST" action="{{ route('login') }}">
-                {{ csrf_field() }}
-                <div class="ui stacked segment">
+                    <h2 class="header">Login</h2>
+
+                    @include('layouts.flashes.form')
+
+                    {{ csrf_field() }}
+
                     <div class="field">
                         <div class="ui left icon input">
                             <i class="user icon"></i>
-                            <input name="email" placeholder="E-mail" type="text" autofocus>
+                            <input name="email" placeholder="E-mail" type="text" value="{{ old('email') }}" autofocus>
                         </div>
                     </div>
+
                     <div class="field">
                         <div class="ui left icon input">
                             <i class="lock icon"></i>
                             <input name="password" placeholder="Contraseña" type="password">
                         </div>
                     </div>
+
                     <button type="submit" class="ui fluid large primary submit button">Login</button>
-                </div>
 
-                {{--<div class="ui error message"></div>--}}
-
-            </form>
-
-            <div class="ui message">
-                <a href="{{ route('password.request') }}">Olvidaste tu contraseña?</a>
+                </form>
             </div>
         </div>
     </div>
-    {{--<div class="container">--}}
-    {{--<div class="row">--}}
-    {{--<div class="col-md-8 col-md-offset-2">--}}
-    {{--<div class="panel panel-default">--}}
-    {{--<div class="panel-heading">Login</div>--}}
-    {{--<div class="panel-body">--}}
-    {{--<form class="form-horizontal" role="form" method="POST" action="{{ route('login') }}">--}}
-    {{--{{ csrf_field() }}--}}
-
-    {{--<div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">--}}
-    {{--<label for="email" class="col-md-4 control-label">E-Mail Address</label>--}}
-
-    {{--<div class="col-md-6">--}}
-    {{--<input id="email" type="email" class="form-control" name="email"--}}
-    {{--value="{{ old('email') }}" required autofocus>--}}
-
-    {{--@if ($errors->has('email'))--}}
-    {{--<span class="help-block">--}}
-    {{--<strong>{{ $errors->first('email') }}</strong>--}}
-    {{--</span>--}}
-    {{--@endif--}}
-    {{--</div>--}}
-    {{--</div>--}}
-
-    {{--<div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">--}}
-    {{--<label for="password" class="col-md-4 control-label">Password</label>--}}
-
-    {{--<div class="col-md-6">--}}
-    {{--<input id="password" type="password" class="form-control" name="password" required>--}}
-
-    {{--@if ($errors->has('password'))--}}
-    {{--<span class="help-block">--}}
-    {{--<strong>{{ $errors->first('password') }}</strong>--}}
-    {{--</span>--}}
-    {{--@endif--}}
-    {{--</div>--}}
-    {{--</div>--}}
-
-    {{--<div class="form-group">--}}
-    {{--<div class="col-md-6 col-md-offset-4">--}}
-    {{--<div class="checkbox">--}}
-    {{--<label>--}}
-    {{--<input type="checkbox"--}}
-    {{--name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me--}}
-    {{--</label>--}}
-    {{--</div>--}}
-    {{--</div>--}}
-    {{--</div>--}}
-
-    {{--<div class="form-group">--}}
-    {{--<div class="col-md-8 col-md-offset-4">--}}
-    {{--<button type="submit" class="btn btn-primary">--}}
-    {{--Login--}}
-    {{--</button>--}}
-
-    {{--<a class="btn btn-link" href="{{ route('password.request') }}">--}}
-    {{--Forgot Your Password?--}}
-    {{--</a>--}}
-    {{--</div>--}}
-    {{--</div>--}}
-    {{--</form>--}}
-    {{--</div>--}}
-    {{--</div>--}}
-    {{--</div>--}}
-    {{--</div>--}}
-    {{--</div>--}}
 @endsection
