42ed559be9f57f84d4188dc3b3e5aa1f20ebcf18
fsarachu <fsarachu@gmail.com>
2017-03-30 18:15:44 -0300

    Add Admin\SurveyController and Admin\SurveyAnswerController, not implemented, just scaffolded

---
+++ app/Http/Controllers/Admin/SurveyAnswerController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\SurveyAnswer;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class SurveyAnswerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\SurveyAnswer  $surveyAnswer
+     * @return \Illuminate\Http\Response
+     */
+    public function show(SurveyAnswer $surveyAnswer)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\SurveyAnswer  $surveyAnswer
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(SurveyAnswer $surveyAnswer)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\SurveyAnswer  $surveyAnswer
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, SurveyAnswer $surveyAnswer)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\SurveyAnswer  $surveyAnswer
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(SurveyAnswer $surveyAnswer)
+    {
+        //
+    }
+}
