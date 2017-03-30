659d893499374104735dfde19b39e12dc4dd886d
fsarachu <fsarachu@gmail.com>
2017-03-30 16:54:06 -0300

    Create migration "create_survey_answers_table"

---
+++ database/migrations/2017_03_30_185205_create_survey_answers_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSurveyAnswersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('survey_answers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('rating');
+            $table->string('extra_comments')->nullable();
+            $table->integer('survey_id')->unsigned();
+            $table->string('user_id')->nullable();
+            $table->timestamps();
+
+            $table->index('survey_id');
+            $table->index('user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('survey_answers');
+    }
+}
