3548e292fe6dac52cfad61ccb735514935a24c82
fsarachu <fsarachu@gmail.com>
2017-03-31 06:25:38 -0300

    Define Survey fillable fields

--- app/Survey.php
+++ app/Survey.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Survey extends Model
 {
+    protected $fillable = ['event_id', 'description', 'question', 'has_extra_comments', 'extra_comments_title'];
+
     public function answers()
     {
         return $this->hasMany(SurveyAnswer::class);
