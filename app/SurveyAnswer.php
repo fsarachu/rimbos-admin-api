e90d2d511074851b2dd88dd5c5f2dbf9955a2cb3
fsarachu <fsarachu@gmail.com>
2017-03-31 09:57:17 -0300

    Define SurveyAnswer fillable fields

--- app/SurveyAnswer.php
+++ app/SurveyAnswer.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class SurveyAnswer extends Model
 {
+    protected $fillable = ['rating', 'extra_comments', 'survey_id', 'user_id'];
+
     public function survey()
     {
         return $this->belongsTo(Survey::class);
