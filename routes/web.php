36d7de5ce1f32bb9189744638ed627f3f6c21d2a
fsarachu <fsarachu@gmail.com>
2017-03-30 23:11:12 -0300

    Reformat routes file

--- routes/web.php
+++ routes/web.php
@@ -11,29 +11,34 @@
 |
 */
 
-/* Admin Routes */
+
+/*
+|--------------------------------------------------------------------------
+| Admin Routes
+|--------------------------------------------------------------------------
+*/
+
 Route::group(['domain' => env('APP_ADMIN_DOMAIN')], function () {
     Route::get('/', 'Admin\HomeController@index')->name('admin.index');
-
     Route::resource('invoices', 'Admin\InvoiceController', ['as' => 'admin']);
     Route::resource('surveys', 'Admin\SurveyController', ['as' => 'admin']);
     Route::resource('surveys.answers', 'Admin\SurveyAnswerController', ['as' => 'admin']);
-
-    Route::get('/storage/{file_name}', 'Admin\FileController')->where(['file_name' => '.*'])->name('admin.storage');
-
     //Auth::routes();
-
     Route::get('/login', 'Auth\LoginController@showLoginForm')->name('login');
-
     Route::post('/login', 'Auth\LoginController@login');
-
     Route::post('/logout', 'Auth\LoginController@logout')->name('logout');
+    Route::get('/storage/{file_name}', 'Admin\FileController')->where(['file_name' => '.*'])->name('admin.storage');
 });
 
-/* Surveys Routes */
+
+/*
+|--------------------------------------------------------------------------
+| Front Routes
+|--------------------------------------------------------------------------
+*/
+
 Route::group(['domain' => env('APP_SURVEY_DOMAIN')], function () {
     Route::get('/auth', 'Front\AuthController@authenticate')->name('survey.auth');
-
     Route::get('/', 'Front\SurveyController@show')->name('survey.show');
     Route::post('/', 'Front\SurveyController@answer')->name('survey.answer');
 });
