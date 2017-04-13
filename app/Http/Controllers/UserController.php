ec5dbe78b7331652a2f5e0e31978667410b70839
fsarachu <fsarachu@gmail.com>
2017-04-13 05:13:38 -0300

    Implement User signup and login

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use JWTAuth;
+
+class UserController extends Controller
+{
+    public function register(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|string|max:50',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|string|confirmed'
+        ]);
+
+        $user = new User([
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'password' => bcrypt($request->input('password'))
+        ]);
+
+        $user->save();
+
+        $token = JWTAuth::fromUser($user);
+
+        return response()->json(compact('message', 'token'), 201);
+    }
+
+    public function authenticate(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|string',
+            'password' => 'required|string'
+        ]);
+
+        $credentials = $request->only(['name', 'password']);
+
+        try {
+            if (!$token = JWTAuth::attempt($credentials)) {
+                return response()->json(['message' => 'Invalid credentials'], 401);
+            }
+        } catch (JWTException $e) {
+            return response()->json(['message' => 'Could not create token'], 500);
+        }
+
+        return response()->json(compact('token'), 200);
+    }
+}
